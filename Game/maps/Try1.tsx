c7e161cf32987ca6ddd0e5be824254d0c4bb8020
Laia <36154539+LaiaMartinezMotis@users.noreply.github.com>
2019-04-04 18:07:08 +0200

    Added buildings select level

---
+++ Game/maps/Try1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Try1" tilewidth="60" tileheight="30" spacing="1" margin="1" tilecount="10" columns="2">
+ <image source="../sprites/Selectlevel Buildings/Try1.png" width="146" height="158"/>
+</tileset>
