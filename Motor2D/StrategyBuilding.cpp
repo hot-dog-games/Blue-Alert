5e3db6e1715bb11860aa8ddf957a9244e90b46b3
Marcgs96 <36154480+Marcgs96@users.noreply.github.com>
2019-04-10 08:51:02 +0200

    uncomment transitions

--- Motor2D/StrategyBuilding.cpp
+++ Motor2D/StrategyBuilding.cpp
@@ -40,9 +40,9 @@ bool StrategyBuilding::Update(float dt)
 				state = STATIC_HOVERED;
 				current_animation = &animations.find("highlight")->second;
 				if (App->input->GetMouseButtonDown(1)) {
-					//App->transition_manager->CreateFadeTransition(2.0f, true, 3, White);
+					App->transition_manager->CreateFadeTransition(2.0f, true, 3, White);
 					App->transition_manager->CreateZoomTransition(2.0f);
-					//App->transition_manager->CreateCameraTranslation(2.0f, { mouse_world.x, mouse_world.y });
+					App->transition_manager->CreateCameraTranslation(2.0f, { mouse_world.x, mouse_world.y });
 					App->game_manager->GetEncounterTree()->SetCurrentNodeByEntity(this);
 					
 				}
