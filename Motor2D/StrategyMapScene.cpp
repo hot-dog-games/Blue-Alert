4585d419460fc2e9f34705cee3ba1f279b5616ca
lakaens <lakaens12@gmail.com>
2019-04-17 11:30:07 +0200

    updating strategy map scene

--- Motor2D/StrategyMapScene.cpp
+++ Motor2D/StrategyMapScene.cpp
@@ -44,18 +44,18 @@ bool StrategyMapScene::Start()
 
 	main_panel = App->gui->CreateImage({ 0,0 }, { 0, 0, (int)w, (int)h }, nullptr, false);
 	banner = App->gui->CreateImage({ 4,5 }, { 1,769,1017,83 }, main_panel);
-	SDL_Rect settings_rect[3];
-	settings_rect[0] = { 0,481,59,51 };
-	settings_rect[1] = { 60,481,58,51 };
-	settings_rect[2] = { 119,481,58,51 };
+	SDL_Rect small_button_rect[3];
+	small_button_rect[0] = { 0,481,59,51 };
+	small_button_rect[1] = { 60,481,58,51 };
+	small_button_rect[2] = { 119,481,58,51 };
 
-	SDL_Rect menu_rect[3];
-	menu_rect[0] = { 0,533,220,51 };
-	menu_rect[1] = { 0,585,220,51 };
-	menu_rect[2] = { 0,637,220,51 };
+	SDL_Rect large_button_rect[3];
+	large_button_rect[0] = { 0,533,220,51 };
+	large_button_rect[1] = { 0,585,220,51 };
+	large_button_rect[2] = { 0,637,220,51 };
 
-	settings_button = App->gui->CreateButton({ 50,700 }, settings_rect, main_panel);
-	menu_button = App->gui->CreateButton({ 700,700 }, menu_rect, main_panel);
+	settings_button = App->gui->CreateButton({ 50,700 }, small_button_rect, main_panel);
+	menu_button = App->gui->CreateButton({ 700,700 }, large_button_rect, main_panel);
 
 	gold = App->gui->CreateLabel({ 90, 30 }, "ui/Fonts/command_and_conquer___logo_font_by_dexistor371-d6k2yvb.ttf", 20, "GOLD", { 0,0,0,0 }, 0, main_panel);
 	energy = App->gui->CreateLabel({ 450, 30 }, "ui/Fonts/command_and_conquer___logo_font_by_dexistor371-d6k2yvb.ttf", 20, "ENERGY", { 0,0,0,0 },0, main_panel);
@@ -119,6 +119,16 @@ bool StrategyMapScene::CleanUp()
 
 bool StrategyMapScene::GUIEvent(UIElement * element, GUI_Event gui_event)
 {
+	SDL_Rect small_button_rect[3];
+	small_button_rect[0] = { 0,481,59,51 };
+	small_button_rect[1] = { 60,481,58,51 };
+	small_button_rect[2] = { 119,481,58,51 };
+
+	SDL_Rect large_button_rect[3];
+	large_button_rect[0] = { 0,533,220,51 };
+	large_button_rect[1] = { 0,585,220,51 };
+	large_button_rect[2] = { 0,637,220,51 };
+
 	if (gui_event == GUI_Event::LEFT_CLICK_DOWN) {
 
 	}
