fc010dd48b2bfc27cbe55cc8c278067d5203b5f8
AxelAlavedra <axel.alavedra@gmail.com>
2019-04-22 22:23:05 +0200

    Update Textures.cpp

--- Motor2D/Textures.cpp
+++ Motor2D/Textures.cpp
@@ -50,11 +50,11 @@ bool Textures::CleanUp()
 
 	for(item = textures.begin(); item != textures.end(); ++item)
 	{
-		SDL_DestroyTexture(item->second);
+		if(item->second)
+			SDL_DestroyTexture(item->second);
 		textures.erase(item);
 	}
 
-	textures.clear();
 	IMG_Quit();
 	return true;
 }
