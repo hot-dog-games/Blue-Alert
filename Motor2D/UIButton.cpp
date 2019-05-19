711ecb7f3a10d8314a6d5f2461ffe4e32d082900
Alex Morales Garcia <alex2521999@github.com>
2019-05-19 23:12:39 +0200

    Commented fx

--- Motor2D/UIButton.cpp
+++ Motor2D/UIButton.cpp
@@ -24,7 +24,7 @@ bool UIButton::UIBlit()
 
 void UIButton::OnMouseClick()
 {
-	App->audio->PlayFx(button_fx.c_str(), 0);
+	//App->audio->PlayFx(button_fx.c_str(), 0);
 	rect_sprite = anim[2];
 }
 
@@ -83,5 +83,5 @@ UIButton::UIButton(iPoint position, SDL_Rect* sprite_rect, bool is_interactable)
 
 	rect_sprite = anim[interactable ? 0 : 3];
 
-	button_fx=App->audio->LoadFx("audio/fx/UI/button_fx.wav");
+	//button_fx=App->audio->LoadFx("audio/fx/UI/button_fx.wav");
 }
