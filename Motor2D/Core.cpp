dfec00fa1ed32003bd57c0e754cf22fd68bd4ebd
AxelAlavedra <axel.alavedra@gmail.com>
2019-06-10 22:37:58 +0200

    Update Core.cpp

--- Motor2D/Core.cpp
+++ Motor2D/Core.cpp
@@ -25,7 +25,7 @@ Core::Core(pugi::xml_node entity_config, fPoint position, Faction faction, pugi:
 	current_animation = &animations.find("idle")->second;
 
 	attack_fx = App->audio->LoadFx("audio/fx/Ambient_Sounds/Shots/basic_shot.wav");
-	App->audio->SetFXVolume(attack_fx.c_str(), 30);
+	App->audio->SetFXVolume(attack_fx.c_str(), App->audio->GetFxVolume());
 }
 
 Core::~Core()
